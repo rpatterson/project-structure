deff4007ba10e075890edadd19e0c7f6afcb687c
Ross Patterson <me@rpatterson.net>
2023-05-13 02:18:56 -0700

    style(lit): Cleanup unused HTML linters

--- .linthtmlrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	"maxerr": false,
-	"text-ignore-regex": false,
-	"raw-ignore-regex": false,
-	"attr-name-ignore-regex": false,
-	"id-class-ignore-regex": false,
-	"line-max-len-ignore-regex": false,
-	"rules": {}
-}
